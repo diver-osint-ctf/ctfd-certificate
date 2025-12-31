48379c1b88e49a69eebf4e33d0ed1b433c905da8
xryuseix <51394682+xryuseix@users.noreply.github.com>
2025-12-31 23:33:17 +0900

    chore: add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+PLUGINS_PATH=${1:-"/home/ctf/CTFd/CTFd/plugins"}
+
+# Install python dependencies
+# Install system dependencies for WeasyPrint
+docker-compose exec -T ctfd apt-get update
+docker-compose exec -T ctfd apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libglib2.0-0
+
+docker-compose exec -T ctfd pip install -r $PLUGINS_PATH/ctfd-certificate/requirements.txt
